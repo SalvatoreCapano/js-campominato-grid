c538d3bfa206d34c0fe9ac5e0204e7734b2f1163
Salvatore Capano <capano_salvatore@libero.it>
2023-01-11 16:56:37 +0100

    Esercizio Completato

--- js/script.js
+++ js/script.js
@@ -8,6 +8,13 @@ playBtn.addEventListener("click", function(){
 
     // Svuota la griglia
     grid.innerHTML = "";
+    // Svuota la console
+    console.clear();
+
+    // Se non presente, mostra la griglia
+    if (!grid.classList.contains("show")) {
+        grid.classList.add("show");
+    }
 
     // Riempie la griglia
     for (let i = 0; i < cellsNumber; i++) {
@@ -17,8 +24,14 @@ playBtn.addEventListener("click", function(){
         newCell.classList.add("cell");
 
         newCell.innerHTML = i + 1;
-        
+
         grid.append(newCell);
+
+        newCell.addEventListener("click", function(){
+            console.log("Hai cliccato la cella", this.innerHTML);
+            this.classList.add("active");
+            
+        });
     }
 
 });
